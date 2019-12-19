69a7f15c544412cb5666843d5b08ef448888172f
Nicolas Boulet <nicolasboulet@live.fr>
2019-12-19 20:55:05 +0100

    Create Restaurant CRUD

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,47 @@
+class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    if @restaurant.destroy
+      redirect_to restaurants_path
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 15 fake restaurants...'
+
+15.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
