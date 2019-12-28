56c0e4c6c5a1e05291cb43744201688d075e443e
Nicolas Boulet <nicolasboulet@live.fr>
2019-12-28 10:05:29 +0100

    Yelp finish

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,6 +1,5 @@
-class RestaurantsController < ApplicationController
-
-  before_action :set_restaurant, only: [:show]
+class Admin::RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def new
     @restaurant = Restaurant.new
@@ -11,22 +10,22 @@ class RestaurantsController < ApplicationController
     @restaurant.save
   end
 
-  # def update
-  #   if @restaurant.update(restaurant_params)
-  #     redirect_to restaurant_path(@restaurant)
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def destroy
-  #   if @restaurant.destroy
-  #     redirect_to restaurants_path
-  #   end
-  # end
+  def destroy
+    if @restaurant.destroy
+      redirect_to admin_restaurants_path
+    end
+  end
 
   def show
     @reviews = @restaurant.reviews
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :restaurants do
+  get 'reviews/new'
+  get 'reviews/create'
+  resources :restaurants, only: [:new, :create, :show, :index] do
     resources :reviews, only: [:new, :create]
+  end
+
+  namespace :admin do
+    resources :restaurants
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
